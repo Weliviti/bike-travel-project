3083629fde06f7fafb0c235b6b36396cf6021455
Weliviti <walivitigodaashen@gmail.com>
2025-11-21 17:21:50 +0530

    Add Navbar, Footer, pages, and global styles

---
+++ src/pages/Footer.tsx
@@ -0,0 +1,20 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <h3>Bike Traveler</h3>
+      <p>
+        Connecting travelers and adventurers across the world.<br />
+        &copy; {new Date().getFullYear()} Bike Traveler. All rights reserved.
+      </p>
+      <div className="social-links">
+        <a href="#">Facebook</a>
+        <a href="#">Instagram</a>
+        <a href="#">Twitter</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "20px", background: "#222", color: "white" }}>
+    <nav className="navbar">
       <h2>Bike Traveler</h2>
-      <ul style={{ display: "flex", gap: "20px", listStyle: "none" }}>
-        <li><Link to="/" style={{ color: "white" }}>Home</Link></li>
-        <li><Link to="/bikes" style={{ color: "white" }}>Used Bikes</Link></li>
-        <li><Link to="/travelled" style={{ color: "white" }}>Travelled Places</Link></li>
-        <li><Link to="/map" style={{ color: "white" }}>Map</Link></li>
-        <li><Link to="/about" style={{ color: "white" }}>About</Link></li>
-        <li><Link to="/contact" style={{ color: "white" }}>Contact</Link></li>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/bikes">Used Bikes</Link></li>
+        <li><Link to="/travelled">Travelled Places</Link></li>
+        <li><Link to="/map">Map</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
